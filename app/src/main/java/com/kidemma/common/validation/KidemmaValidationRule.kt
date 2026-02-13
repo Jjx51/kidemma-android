fe1ef5434d835cabc46db54384917b042828b95c
José Carrillo <61214458+JoeMan97@users.noreply.github.com>
2026-02-13 11:26:43 -0600

    :sparkles: (Validation) : Implemented a field validation system with supporting localized strings and unit tests (#8)

---
+++ app/src/main/java/com/kidemma/common/validation/KidemmaValidationRule.kt
@@ -0,0 +1,8 @@
+package com.kidemma.common.validation
+
+sealed interface KidemmaValidationRule {
+    data object Required : KidemmaValidationRule
+    data object Optional : KidemmaValidationRule
+    data class MinLength(val length: Int) : KidemmaValidationRule
+    data object EmailFormat : KidemmaValidationRule
+}
